493d58a977c6b30aff543d0c0495ac35a8c2ba23
Анжелика <lika.smkina@bk.ru>
2025-04-13 20:45:33 +0500

    Update pinpong.py

--- pinpong.py
+++ pinpong.py
@@ -1,9 +1,8 @@
 from pygame import *
 from random import randint
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, size_x=65, size_y=65):
+    def __init__(self, player_image, player_x, player_y, player_speed,):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65, 65))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -13,20 +12,31 @@ class GameSprite(sprite.Sprite):
 win_wight = 700
 win_height = 500
 
-
-
+image = Surface((700,500))
+image.fill((127, 255, 212))
+rect = image.get_rect()
 
 window = display.set_mode((700, 500))
 display.set_caption('pinpong')
 
-
+game = True
+FPS = 60
 
 
 class Player(GameSprite):
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
+        if keys[K_UP] and self.rect.x > 5:
             self.rect.x -= self.speed
         
-        if keys[K_RIGHT] and self.rect.x < 700 - 80:
+        if keys[K_DOWN] and self.rect.x < 700 - 80:
             self.rect.x += self.speed 
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        
+        if keys[K_s] and self.rect.x < 700 - 80:
+            self.rect.x += self.speed 
+
